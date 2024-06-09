a214d83dd35c6aaac244dd091ee4f4fa0976efa2
k-redstone <che0618@naver.com>
2024-06-09 20:11:17 +0900

    Test: dev 테스트
    - #5

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <div className="h-[244px] bg-slate-500">
       <ContentLayout>
         <h1 className="">푸터영역 입니다.</h1>
+        <h1 className="">테스트입니다.</h1>
       </ContentLayout>
     </div>
   );
