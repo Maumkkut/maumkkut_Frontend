b770b12c1484a853fca194747043d2ba32d05488
k-redstone <che0618@naver.com>
2024-09-25 21:06:30 +0900

    Fix: 프로필 페이지에서 속한 그룹이 없다면 not-content 보여주도록 수정

--- src/pages/ProfilePage/RigthSidePage/GroupManage.tsx
+++ src/pages/ProfilePage/RigthSidePage/GroupManage.tsx
@@ -1,10 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 import { useFetchMyGroupList } from '@/hooks/queries/group';
 
+import LoadingPage from '@/pages/LoadingPage';
 export default function GroupManage() {
   const navigate = useNavigate();
 
-  const { data } = useFetchMyGroupList();
+  const { data, isSuccess } = useFetchMyGroupList();
+
+  if (!isSuccess) {
+    return <LoadingPage />;
+  }
 
   return (
     <div className="flex w-[800px] flex-col gap-y-5">
@@ -16,7 +21,7 @@ export default function GroupManage() {
       {/* 이게 진짜임 */}
       <div className="flex flex-col">
         {/* 컴포넌트 하나 */}
-        {data &&
+        {data ? (
           data.result.map((item) => (
             <div
               key={item.id}
@@ -50,26 +55,13 @@ export default function GroupManage() {
                 </button>
               </div> */}
             </div>
-          ))}
-
-        {/* <div className="flex items-center justify-between border-y px-4 py-7">
-          <div className="flex items-center gap-x-5 text-mk-darkgray">
-            <div className="flex h-[50px] w-[75px] items-center justify-center rounded-lg bg-mk-logo0 text-center">
-              <span>D-16</span>
-            </div>
-            <p className="text-3xl">아이고두야(4명)</p>
+          ))
+        ) : (
+          <div className="mt-20 grow text-center text-xl text-mk-logo4">
+            <p>속한 그룹이 없습니다.</p>
+            <p>그룹 만들기로 그룹을 만들어보세요!</p>
           </div>
-
-          <p className="text-2xl text-mk-logo3">일정 확인</p>
-          <div className="flex flex-col gap-y-5">
-            <button className="rounded-lg bg-mk-logo3 px-10 py-4">
-              <span className="text-white">수정</span>
-            </button>
-            <button className="rounded-lg border border-mk-logo3 px-10 py-4">
-              <span className="text-mk-darkgray">삭제</span>
-            </button>
-          </div>
-        </div> */}
+        )}
       </div>
     </div>
   );
